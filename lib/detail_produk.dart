1934cef01c2f72560db907fe7ce599b3a533e620
Tiksna Apsari <ayutiksnaapsr@gmail.com>
2025-06-12 02:35:27 +0800

    create detail_produk

---
+++ lib/detail_produk.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ProductDetail extends StatelessWidget {
+  const ProductDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
