9a178de30897645d2f865eb8a4a2aeb6162c6517
KadekDwiki <nusanjayadwiki@gmail.com>
2025-06-11 12:58:28 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/nest_and_beans/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nest_and_beans
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.dark),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.interTextTheme(),
+        colorScheme: ColorScheme.light(
+          primary: Color(0xFF185221),
+          secondary: Color.fromRGBO(117, 117, 117, 0.7),
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colorscheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(title: const Text('Nest & Beans')),
+      body: Center(
+        child: Text(
+          'Hello Nest & Beans!',
+          style: TextStyle(fontSize: 24, color: colorscheme.primary),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
