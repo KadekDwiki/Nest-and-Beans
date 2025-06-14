07e01d7b639a7fce7277940266f69687b31edcba
bydhrma <bayudharmap@gmail.com>
2025-06-14 12:55:57 +0800

    adding depedencies, homepage, and assets

---
+++ lib/homepage.dart
@@ -0,0 +1,557 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF6F6F6), 
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            HeaderWithSearch(),    
+            SizedBox(height: 45),
+            VoucherCard(),
+            SizedBox(height: 25),
+            PromoCarousel(),
+            SizedBox(height: 25),
+            RekomendasiProduk(),
+            SizedBox(height: 25),
+            ListProduk(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: CustomBottomNav(), 
+    );
+  }
+}
+
+class HeaderWithSearch extends StatelessWidget {
+  const HeaderWithSearch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [ 
+        Container(
+          padding: const EdgeInsets.only(top: 92, left: 42, right: 42, bottom: 52),
+          decoration: const BoxDecoration(
+            color: Color(0xFF005E2E),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            )
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                  Text("Selamat Siang,", style: TextStyle(color: Colors.white , fontWeight: FontWeight.bold,fontSize: 24)),
+                  Text("Mutiara Dianing", style: TextStyle(color: Colors.white,  fontSize: 18)),
+                  ],
+              ),
+              Row(
+                children: const [
+                  Icon(Icons.shopping_cart_outlined, color: Colors.white, size: 32,),
+                ],
+              ),
+            ],
+          ),
+        ),
+
+        Positioned(
+          bottom: -24,
+          left: 28,
+          right: 28,
+          child: Material(
+            elevation: 10,
+            shadowColor: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.2),
+            borderRadius: BorderRadius.circular(12),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Cari makanan favoritmu!',
+                prefixIcon: const Icon(Icons.search),
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.symmetric(vertical: 14),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(70),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class VoucherCard extends StatelessWidget {
+  const VoucherCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30),
+      padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 20,
+            offset: Offset(0, 1),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          // Icon toko
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: const Color(0xFFF2F4E7),
+              shape: BoxShape.circle,
+            ),
+            child: const Icon(Icons.card_giftcard, color: Color(0xFF005E2E)),
+          ),
+          const SizedBox(width: 12),
+          // Info lokasi
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Voucher Untukmu!',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                ),
+                SizedBox(height: 4),
+                Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Tukarkan Poinmu Dan Dapatkan Voucher!',
+                        style: TextStyle(color: Colors.green, fontSize: 10),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Icon(Icons.chevron_right, color: Colors.grey),
+        ],
+      ),
+    );
+  }
+}
+
+class PromoCarousel extends StatefulWidget {
+  const PromoCarousel({super.key});
+
+  @override
+  State<PromoCarousel> createState() => _PromoCarouselState();
+}
+
+class _PromoCarouselState extends State<PromoCarousel> {
+  final List<String> imageList = [
+    'assets/images/banner1.png',
+    'assets/images/banner1.png',
+    'assets/images/banner1.png',
+  ];
+
+  int _current = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0), 
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 140.0,
+                enlargeCenterPage: false,
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 4),
+                viewportFraction: 1.0,
+                enableInfiniteScroll: true,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                },
+              ),
+              items: imageList.map((imagePath) {
+                return Image.asset(
+                  imagePath,
+                  fit: BoxFit.fill,
+                  width: double.infinity,
+                );
+              }).toList(),
+            ),
+            Positioned(
+              bottom: 8,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: imageList.asMap().entries.map((entry) {
+                  return Container(
+                    width: 8.0,
+                    height: 8.0,
+                    margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _current == entry.key
+                          ? const Color(0xFF005E2E)
+                          : Colors.white.withOpacity(0.5),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RekomendasiProduk extends StatelessWidget {
+  const RekomendasiProduk({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, String>> products = [
+      {
+        'title': 'Almond Croissant',
+        'desc': 'Croissant dengan taburan almond...',
+        'image': 'assets/images/produk.png',
+        'price': 'Rp 36.000',
+      },
+      {
+        'title': 'Cappuccino',
+        'desc': 'Kopi hangat dengan susu steamed',
+        'image': 'assets/images/produk.png',
+        'price': 'Rp 25.000',
+      },
+      {
+        'title': 'Avocado Juice',
+        'desc': 'Jus alpukat segar dan kental',
+        'image': 'assets/images/produk.png',
+        'price': 'Rp 22.000',
+      },
+    ];
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Recommended',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Lihat Semua',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Color(0xFF005E2E),
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 6),
+        SizedBox(
+          height: 120,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            itemCount: products.length,
+            itemBuilder: (context, index) {
+              final product = products[index];
+              return Container(
+                width: 280,
+                margin: const EdgeInsets.only(right: 16),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  elevation: 20,
+                  shadowColor: Colors.black.withOpacity(0.2),
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(6),
+                          bottomLeft: Radius.circular(6),
+                        ),
+                        child: Image.asset(
+                          product['image']!,
+                          width: 80,
+                          height: 120,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      product['title']!,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                product['desc']!,
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              const SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    product['price']!,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                      color: Color(0xFF005E2E)
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 28,
+                                    height: 28,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xFF005E2E),
+                                      borderRadius: BorderRadius.circular(5),  
+                                      
+                                      ),
+                                    child: const Icon(Icons.add, size: 16, color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ListProduk extends StatelessWidget {
+  const ListProduk({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, String>> products = [
+      {
+        'title': 'Cafe Malt Latte',
+        'desc': 'Latte dengan malt yang creamy',
+        'image': 'assets/images/produk.png',
+        'price': 'Rp 23.000',
+      },
+      {
+        'title': 'Americano Aren',
+        'desc': 'Americano dengan gula aren',
+        'image': 'assets/images/produk.png',
+        'price': 'Rp 19.000',
+      },
+      {
+        'title': 'Americano Aren',
+        'desc': 'Americano dengan gula aren',
+        'image': 'assets/images/produk.png',
+        'price': 'Rp 19.000',
+      },
+      {
+        'title': 'Americano Aren',
+        'desc': 'Americano dengan gula aren',
+        'image': 'assets/images/produk.png',
+        'price': 'Rp 19.000',
+      },
+      {
+        'title': 'Americano Aren',
+        'desc': 'Americano dengan gula aren',
+        'image': 'assets/images/produk.png',
+        'price': 'Rp 19.000',
+      },
+      {
+        'title': 'Americano Aren',
+        'desc': 'Americano dengan gula aren',
+        'image': 'assets/images/produk.png',
+        'price': 'Rp 19.000',
+      }
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Header Produk Kami
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Produk Kami',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Lihat Selengkapnya',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFF005E2E),
+                ),
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 12), // Spacer kecil
+
+          // Grid Produk
+          GridView.builder(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: products.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 0.80, // lebih seimbang
+            ),
+            itemBuilder: (context, index) {
+              final product = products[index];
+              return Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                 elevation: 20,
+                shadowColor: Colors.black.withOpacity(0.2),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Gambar dengan rasio tetap
+                    ClipRRect(
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(12),
+                      ),
+                      child: AspectRatio(
+                        aspectRatio: 1.6, // Rasio lebih pendek agar tidak terlalu tinggi
+                        child: Image.asset(
+                          product['image']!,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    // Konten teks
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product['title']!,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            product['desc']!,
+                            style: const TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            product['price']!,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 13,
+                              color: Color(0xFF005E2E),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class CustomBottomNav extends StatelessWidget {
+  const CustomBottomNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedItemColor: const Color(0xFF005E2E),
+      unselectedItemColor: Colors.grey,
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Beranda'),
+        BottomNavigationBarItem(icon: Icon(Icons.receipt_long), label: 'Pesananku'),
+        BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Komunitas'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Akun'),
+      ],
+    );
+  }
+}
+
+
