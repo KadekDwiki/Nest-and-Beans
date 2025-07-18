b99b160e5ccc1b574483bfe97ae36a77a99b1fae
pyarivisva <pyarivisvapujita1@gmail.com>
2025-07-18 17:05:27 +0800

    fix error providers

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => HomeProvider(),
+      create: (_) => ReviewProvider(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
