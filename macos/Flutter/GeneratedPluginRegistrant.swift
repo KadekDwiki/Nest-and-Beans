698100babdbf4c4fc2b0229b1a22837ccb515faa
IndriAnisaPutri <indrianisaputri14@gmail.com>
2025-07-18 12:06:02 +0800

    pubspec.lock

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,18 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
+import audioplayers_darwin
 import file_selector_macos
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-=======
-import audioplayers_darwin
-import path_provider_foundation
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
->>>>>>> 170287d66166ca62ba505fd7df9255914f70cc99
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,17 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
-<<<<<<< HEAD
-#include <file_selector_windows/file_selector_windows.h>
-
-void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FileSelectorWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FileSelectorWindows"));
-=======
 #include <audioplayers_windows/audioplayers_windows_plugin.h>
+#include <file_selector_windows/file_selector_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AudioplayersWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
->>>>>>> 170287d66166ca62ba505fd7df9255914f70cc99
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
 }
